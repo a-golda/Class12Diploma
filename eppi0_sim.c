a8ef25528c55062d3b31cf24433502c83f0e8429
golodovka <mr.andrey.golda@mail.ru>
2020-11-25 01:09:21 +0300

    fine with tabs and rm some other shit

--- eppi0_sim.c
+++ eppi0_sim.c
@@ -20,9 +20,7 @@
 using namespace clas12;
 
 void eppi0_sim()
-{
-  // Record start time
-   
+{  
 auto db=TDatabasePDG::Instance();
 
 TLorentzVector beam(0,0,6.535,6.535); //Объявление четырехвекторов, описывающих кинематику
@@ -61,16 +59,16 @@ std::cout << " reading file example program (HIPO) " << std::endl;
 
 vector<string> data;	
 
-	data.push_back("out_out1.hipo");
-	// data.push_back("out_out2.hipo");
-	// data.push_back("out_out3.hipo");
-	// data.push_back("out_out4.hipo");
-	// data.push_back("out_out5.hipo");
-	// data.push_back("out_out6.hipo");
-	// data.push_back("out_out7.hipo");
-	// data.push_back("out_out8.hipo");
-	// data.push_back("out_out9.hipo");
-	// data.push_back("out_out10.hipo");
+data.push_back("out_out1.hipo");
+// data.push_back("out_out2.hipo");
+// data.push_back("out_out3.hipo");
+// data.push_back("out_out4.hipo");
+// data.push_back("out_out5.hipo");
+// data.push_back("out_out6.hipo");
+// data.push_back("out_out7.hipo");
+// data.push_back("out_out8.hipo");
+// data.push_back("out_out9.hipo");
+// data.push_back("out_out10.hipo");
 
 for(int r=0;r<data.size();r++)
 { 
@@ -114,8 +112,8 @@ for(int r=0;r<data.size();r++)
     int sectg=-10;
 
 
-////////////////////////////////////////////////////////////////////generated events///////////////////////////////////////////////////////////////////////////////////////
 
+    //generated events
     elmc.SetXYZM(PARTMC.getFloat("px",0),PARTMC.getFloat("py",0),PARTMC.getFloat("pz",0),0);
     qmc=beam-elmc;
     Wmc=beam-elmc+target;
@@ -125,91 +123,86 @@ for(int r=0;r<data.size();r++)
     {
       int pidmc = PARTMC.getInt("pid",i);
       if(pidmc==2212) prmc.SetXYZM(PARTMC.getFloat("px",i),PARTMC.getFloat("py",i),PARTMC.getFloat("pz",i),0.9383); 
-      if(pidmc==22){
-      if(ngammc==0) {g1mc.SetXYZM(PARTMC.getFloat("px",i),PARTMC.getFloat("py",i),PARTMC.getFloat("pz",i),0.); ngammc++;}
-        else  g2mc.SetXYZM(PARTMC.getFloat("px",i),PARTMC.getFloat("py",i),PARTMC.getFloat("pz",i),0.);
-    }
-  }
-
-  if(g2mc.E()<g1mc.E()){gbigmc=g1mc; g1mc=g2mc; g2mc=gbigmc;}
-  pi0mc=g1mc+g2mc;
-
-/////////////////////////////////////////////////////////////////reconstructed events////////////////////////////////////////////////////////////////////////////////////////
-
-  el.SetXYZM(PART.getFloat("px",0),PART.getFloat("py",0),PART.getFloat("pz",0),0);
-  q=beam-el;
-  W=beam-el+target;
-
-//заполнение векторов с номерами частиц
-
-  for (int i = 0; i < nrows; i++)                            //без отбора событий, где протон, электрон и гаммы регистрируются в одном секторе
-  {
-    int pid = PART.getInt("pid",i);
-    if(pid==11) electron.push_back(i);
-    if(pid==2212) proton.push_back(i);
-    if(pid==22) gamma.push_back(i);
-  }					
-
-  if(electron.size()==1 && proton.size()==1 && gamma.size()>=2)
-  {                       //two gamma selection
-    pr.SetXYZM(PART.getFloat("px",proton[0]),PART.getFloat("py",proton[0]),PART.getFloat("pz",proton[0]),0.938);
-    for(int k=0; k<gamma.size(); k++)
-    {                                                 //выбор пары гамма-квантов с максимальной энергией
-      Egamtest1.SetXYZM(PART.getFloat("px",gamma[k]),PART.getFloat("py",gamma[k]),PART.getFloat("pz",gamma[k]),0);
-      if(Egamtest1.E()<0) continue;
-      for(int j=k+1; j<gamma.size(); j++)
+      if(pidmc==22)
       {
-        Egamtest2.SetXYZM(PART.getFloat("px",gamma[j]),PART.getFloat("py",gamma[j]),PART.getFloat("pz",gamma[j]),0);
-        if(Egamtest2.E()<0) continue;
-        if(MaxEpi0<Egamtest1.E()+Egamtest2.E()) 
+        if(ngammc==0)
         {
-          Numgammax1=k; Numgammax2=j; MaxEpi0=Egamtest1.E()+Egamtest2.E();
-        }
+          g1mc.SetXYZM(PARTMC.getFloat("px",i),PARTMC.getFloat("py",i),PARTMC.getFloat("pz",i),0.);
+          ngammc++;
+        }else  g2mc.SetXYZM(PARTMC.getFloat("px",i),PARTMC.getFloat("py",i),PARTMC.getFloat("pz",i),0.);
       }
     }
-    if(Numgammax1==-10 || Numgammax2==-10) continue;
 
-    g1.SetXYZM(PART.getFloat("px",gamma[Numgammax1]),PART.getFloat("py",gamma[Numgammax1]),PART.getFloat("pz",gamma[Numgammax1]),0);
-    g2.SetXYZM(PART.getFloat("px",gamma[Numgammax2]),PART.getFloat("py",gamma[Numgammax2]),PART.getFloat("pz",gamma[Numgammax2]),0);
-    if(g2.E()<g1.E()) 
+    if(g2mc.E()<g1mc.E()){gbigmc=g1mc; g1mc=g2mc; g2mc=gbigmc;}
+    pi0mc=g1mc+g2mc;
+
+
+    //reconstructed events
+    el.SetXYZM(PART.getFloat("px",0),PART.getFloat("py",0),PART.getFloat("pz",0),0);
+    q=beam-el;
+    W=beam-el+target;
+
+    //заполнение векторов с номерами частиц
+
+    for (int i = 0; i < nrows; i++) //без отбора событий, где протон, электрон и гаммы регистрируются в одном секторе
     {
-      g1.SetXYZM(PART.getFloat("px",gamma[Numgammax2]),PART.getFloat("py",gamma[Numgammax2]),PART.getFloat("pz",gamma[Numgammax2]),0);
-      g2.SetXYZM(PART.getFloat("px",gamma[Numgammax1]),PART.getFloat("py",gamma[Numgammax1]),PART.getFloat("pz",gamma[Numgammax1]),0);
-    }
-    pi0 = g1+g2;
-    missPi0 = beam+target-el-pr;
-    angle_g1_g2=g1.Angle(g2.Vect())*180.0/TMath::Pi(); 
-  }
+      int pid = PART.getInt("pid",i);
+      if(pid==11) electron.push_back(i);
+      if(pid==2212) proton.push_back(i);
+      if(pid==22) gamma.push_back(i);
+    }					
 
-//////////////////////////////////////////////////////////CUTS///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  if(electron.size()==1 && proton.size()==1 && gamma.size()>1 && W.M()>1.07 && el.E()>1.0 && g1.Angle(el.Vect())*180.0/TMath::Pi()>7 && g2.Angle(el.Vect())*180.0/TMath::Pi()>7 && g1.Theta()*180.0/TMath::Pi()>5 && g2.Theta()*180.0/TMath::Pi()>5 && pi0.M()>0.05 && pi0.M()<0.35 && angle_g1_g2>8 && pi0.E()-(bea +target-el-pr).E()>-0.35 && pi0.E()-(beam+target-el-pr).E()<0.35 && pi0.Angle((beam+target-el-pr).Vect())*180.0/TMath::Pi()<15)
-  {
-  }
-
-
-
-
-
-  if(abs(g1mc.E() - g1.E())<0.1 && abs(g2mc.E() - g2.E())<0.1)
-  {
-    result.push_back(1);
-  } else{
-          result.push_back(0);  
+    if(electron.size()==1 && proton.size()==1 && gamma.size()>=2)
+    {                       //two gamma selection
+      pr.SetXYZM(PART.getFloat("px",proton[0]),PART.getFloat("py",proton[0]),PART.getFloat("pz",proton[0]),0.938);
+      for(int k=0; k<gamma.size(); k++)
+      {                                                 //выбор пары гамма-квантов с максимальной энергией
+        Egamtest1.SetXYZM(PART.getFloat("px",gamma[k]),PART.getFloat("py",gamma[k]),PART.getFloat("pz",gamma[k]),0);
+        if(Egamtest1.E()<0) continue;
+        for(int j=k+1; j<gamma.size(); j++)
+        {
+          Egamtest2.SetXYZM(PART.getFloat("px",gamma[j]),PART.getFloat("py",gamma[j]),PART.getFloat("pz",gamma[j]),0);
+          if(Egamtest2.E()<0) continue;
+          if(MaxEpi0<Egamtest1.E()+Egamtest2.E()) 
+          {
+            Numgammax1=k; Numgammax2=j; MaxEpi0=Egamtest1.E()+Egamtest2.E();
+          }
         }
-  if (out.is_open())
-    {   
-      out<< g1mc.E()<<','<< g2mc.E()<< ',' << result[result.size() - 1] << std::endl;
+      }
+      if(Numgammax1==-10 || Numgammax2==-10) continue;
+
+      g1.SetXYZM(PART.getFloat("px",gamma[Numgammax1]),PART.getFloat("py",gamma[Numgammax1]),PART.getFloat("pz",gamma[Numgammax1]),0);
+      g2.SetXYZM(PART.getFloat("px",gamma[Numgammax2]),PART.getFloat("py",gamma[Numgammax2]),PART.getFloat("pz",gamma[Numgammax2]),0);
+      if(g2.E()<g1.E()) 
+      {
+        g1.SetXYZM(PART.getFloat("px",gamma[Numgammax2]),PART.getFloat("py",gamma[Numgammax2]),PART.getFloat("pz",gamma[Numgammax2]),0);
+        g2.SetXYZM(PART.getFloat("px",gamma[Numgammax1]),PART.getFloat("py",gamma[Numgammax1]),PART.getFloat("pz",gamma[Numgammax1]),0);
+      }
+      pi0 = g1+g2;
+      missPi0 = beam+target-el-pr;
+      angle_g1_g2=g1.Angle(g2.Vect())*180.0/TMath::Pi(); 
+    }
+
+    if(abs(g1mc.E() - g1.E())<0.1 && abs(g2mc.E() - g2.E())<0.1)
+    {
+      result.push_back(1);
+    } else{
+            result.push_back(0);  
+          }
+    if (out.is_open())
+      {   
+        out<< g1mc.E()<<','<< g2mc.E()<< ',' << result[result.size() - 1] << std::endl;
+      } 
+
     } 
+  }
 
-  } 
-}
+  gBenchmark->Stop("timer");
+  gBenchmark->Print("timer");
 
-gBenchmark->Stop("timer");
-gBenchmark->Print("timer");
+  auto finish = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = finish - start;
+  std::cout << "Elapsed time: " << elapsed.count()<< " events = "<<counter<< " s\n";
 
-auto finish = std::chrono::high_resolution_clock::now();
-std::chrono::duration<double> elapsed = finish - start;
-std::cout << "Elapsed time: " << elapsed.count()<< " events = "<<counter<< " s\n";
-
-out.close();
+  out.close();
 }
